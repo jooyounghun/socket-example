cbfb315d5df44fecdd79347135c1d9bab5ef6753
jooyounghun <jymk04130@gmail.com>
2021-03-01 01:27:05 +0900

    add db connection

--- echo-client.py
+++ echo-client.py
@@ -1,4 +1,5 @@
 import socket
+import pymysql
 
 HOST = '127.0.0.1'
 PORT = 9999
@@ -8,7 +9,17 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client_socket.connect((HOST, PORT))
 
-client_socket.sendall('안녕'.encode())
+conn = pymysql.connect(host='localhost', user='root', password='ahfQkd12', db ='study_db', charset='utf8')
+
+curs = conn.cursor()
+
+sql = "select * from test"
+curs.execute(sql)
+
+rows = curs.fetchall()
+
+for row in rows:
+    client_socket.sendall(row[0])
 
 data = client_socket.recv(1024)
 print('Received', repr(data.decode()))
